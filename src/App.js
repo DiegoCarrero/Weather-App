7db36d5c4ef46f954ea5e285002097c2c6ca7c43
Diegocarrero <diegocar2413@gmail.com>
2023-03-28 14:43:34 -0400

    create fetch component and add getWeather function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
+import { getWeather } from './api/fetch';
 import './App.css';
 
 function App() {
+
+  useEffect(
+    getWeather().then(response => console.log(response))
+  ,[])
+
   return (
     <div className="App">
       
