8eea839fae86d3060fb8888a10a0ff282167ca5a
Diegocarrero <diegocar2413@gmail.com>
2023-03-28 17:28:17 -0400

    add state variables to Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,19 @@
 import Header from "../weather/Header"
+import getWeather from '../../api/fetch'
+import { useState } from "react"
 
 export default function Home() {
 
-    useEffect(() => {
-        getWeather().then(response => console.log(response))
-      } ,[])
+    const [search, setSearch] = useState({});
+    const [prevSearches, setPrevSearches] = useState([]);
+
+    // useEffect(() => {
+    //     getWeather().then(response => console.log(response))
+    // } ,[])
 
     return (
         <div>
-            
+            <Header />
         </div>
     )
 }
