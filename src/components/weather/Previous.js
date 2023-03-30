c2357640ef6c0eaebf049cea207cbe9496c6ec0d
Diegocarrero <diegocar2413@gmail.com>
2023-03-30 07:02:08 -0400

    add links but not yet working properly

--- src/components/weather/Previous.js
+++ src/components/weather/Previous.js
@@ -1,11 +1,11 @@
-
+import { Link } from "react-router-dom"
 
 export default function Previous({ prevSearches /*, prevCities*/ }) {
 
     // let currentSearch = prevSearches[prevSearches.length - 1]
     // let feelsLike = search.current_condition[0].FeelsLikeF;
     console.log(prevSearches)
-    
+    let prevCities = []
 
     return (
         <aside>
@@ -14,11 +14,15 @@ export default function Previous({ prevSearches /*, prevCities*/ }) {
                 prevSearches.length > 0 ? 
                     <ul>
                         {
-                            prevSearches.map(prevSearch => (
-                                <li key={prevSearch.nearest_area[0].areaName[0].value}>
-                                    {prevSearch.nearest_area[0].areaName[0].value} - {prevSearch.current_condition[0].FeelsLikeF}°F
-                                </li>
-                            ))
+                            prevSearches.map(prevSearch => {
+                                let cityName = prevSearch.nearest_area[0].areaName[0].value
+                                prevCities.push(cityName)
+                                return (
+                                    <li key={cityName}>
+                                        <Link to={`/${prevCities[prevCities.length - 1]}`}>{cityName}</Link> - {prevSearch.current_condition[0].FeelsLikeF}°F
+                                    </li>
+                                )
+                            })
                         }
                     </ul>
                 : <p>No previous searches</p>
